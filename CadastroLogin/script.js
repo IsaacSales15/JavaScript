e0419d15857192796d3f874935c5637215875091
Isaac Sales Rodrigues <124395470+IsaacSales15@users.noreply.github.com>
2024-06-05 21:48:27 -0300

    Updates, just updates

--- CadastroLogin/script.js
+++ CadastroLogin/script.js
@@ -1,18 +1,17 @@
-function cadastrar() {
-    var novoUsuario = document.getElementById('cadastroUser').value;
-    var novaSenha = document.getElementById('cadastroPassword').value;
+document.getElementById("Cadastrar").addEventListener('click', function(){
+
+    var newUser = document.getElementById('cadastroUser').value;
+    var newPassword = document.getElementById('cadastroPassword').value;
 
     var users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push({ username: novoUsuario, password: novaSenha });
+    users.push({ username: newUser, password: newPassword });
     localStorage.setItem('users', JSON.stringify(users));
 
     alert('Cadastro realizado com sucesso!');
-}
+})
 
 document.addEventListener('DOMContentLoaded', function() {
-    var loginForm = document.getElementById('loginForm');
-
-    loginForm.addEventListener('submit', function(event) {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {;
         event.preventDefault();
 
         var username = document.getElementById('username').value;
